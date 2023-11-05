d5b9107dfbd869f78d767cee9e2c708b246bbda9
vanity <731166814@qq.com>
2023-11-05 20:18:08 +0800

    关系抽取flask框架v1.3版

--- Relation_Extraction/main.py
+++ Relation_Extraction/main.py
@@ -3,8 +3,8 @@ from utils.to_test_fine_grad import text_process
 from utils.auto_extraction import test
 app = Flask(__name__)
 
-@app.route('/ner', methods=['POST'])
-def ner():
+@app.route('/re', methods=['POST'])
+def re():
     text = request.json.get('text', '')
     entities = request.json.get('entities', [])
     ent1_list,ent2_list,text_list,ent1_id,ent2_id= text_process(text,entities)
