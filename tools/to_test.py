c2b8d4937266718af931f156077429d2692ed6c6
vanity <731166814@qq.com>
2023-11-18 18:21:01 +0800

    增加test文件处理

--- tools/to_test.py
+++ tools/to_test.py
@@ -5,7 +5,8 @@ import re
 def NER_text_process(line):
     pattern = r'(？|\?|!|。|！)'
     text = line
-    sub_splited_text = re.sub(r"\s|\r","",text)
+    sub_splited_text = re.sub(r"[\s,\r]","",text)
+    sub_splited_text = re.sub(r"[􀦋,􀤋]","",sub_splited_text)
     #print(splited_text)
     sub_splited_text = re.split(pattern,sub_splited_text)
     splited_text = re.split(pattern,text)
